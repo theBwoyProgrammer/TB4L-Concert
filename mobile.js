ae292474b1bdbe97b4252dadaf0fc612a8a1bb30
Codeaddictjaver <102610715+Javernick@users.noreply.github.com>
2022-06-28 17:47:39 +0300

    commit on capstone-1

--- mobile.js
+++ mobile.js
@@ -1,5 +1,6 @@
 const menu = document.querySelector('.menu-3');
 const hamburger = document.querySelector('.hamburger');
+const link = document.querySelectorAll('.port');
 hamburger.addEventListener('click', () => {
   if (hamburger.classList.contains('active')) {
     hamburger.classList.remove('active');
@@ -8,6 +9,18 @@ hamburger.addEventListener('click', () => {
     hamburger.classList.add('active');
     menu.classList.add('active');
   }
+});
+
+link.forEach((e) => {
+  e.addEventListener('click', () => {
+    if (hamburger.classList.contains('active')) {
+      hamburger.classList.remove('active');
+      menu.classList.remove('active');
+    } else {
+      hamburger.classList.add('active');
+      menu.classList.add('active');
+    }
+  });
 });
 // const menu = document.querySelector('.menu-3');
 // menu.addEventListener('click', () => {
